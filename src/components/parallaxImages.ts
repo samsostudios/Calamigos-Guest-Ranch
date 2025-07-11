469f263995e574dd042fd9bd6fc4d80895bc14c7
samsostudios <sam@samsostudios.com>
2025-07-11 11:56:27 -0600

    removed scroll markers from parallax images

--- src/components/parallaxImages.ts
+++ src/components/parallaxImages.ts
@@ -34,7 +34,7 @@ class ParallaxImages {
           start: 'top bottom',
           end: 'bottom top',
           scrub: true,
-          markers: true,
+          // markers: true,
         },
       });
 
